20743d5fdfc8b5841e9d6daeee2141f04ad69c05
jpminh <jpminh@1c8f035f-1d2c-0410-b344-350af37c1d99>
2007-03-22 09:32:25 +0000

    Ejb et classe pour la generation de Maven

---
+++ DragonflyEjb/src/main/java/fr/umlv/dragonflyEJB/services/project/maven/DependencyInformation.java
@@ -0,0 +1,61 @@
+/*
+ * DependencyInformation.java
+ *
+ * Created on 22 mars 2007, 00:02
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package fr.umlv.dragonflyEJB.services.project.maven;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author J-Paul
+ */
+public class DependencyInformation implements Serializable{
+    
+    private String groupId;
+    private String artifactId;
+    private String version;
+    private String scope;
+    
+    /** Creates a new instance of DependencyInformation */
+    public DependencyInformation() {
+    }
+
+    public String getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(String groupId) {
+        this.groupId = groupId;
+    }
+
+    public String getArtifactId() {
+        return artifactId;
+    }
+
+    public void setArtifactId(String artifactId) {
+        this.artifactId = artifactId;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getScope() {
+        return scope;
+    }
+
+    public void setScope(String scope) {
+        this.scope = scope;
+    }
+    
+}

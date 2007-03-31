389d73d5705b1ddd1dc0e40ed4204d0750b928a4
tavarespatrick01 <tavarespatrick01@1c8f035f-1d2c-0410-b344-350af37c1d99>
2007-03-31 10:33:53 +0000

    Rajout suppresion repertoire qd supp projet

--- versionbddchanger/DragonflyEjb/src/main/java/fr/umlv/dragonflyEJB/locals/dropper/DropperManagerBean.java
+++ versionbddchanger/DragonflyEjb/src/main/java/fr/umlv/dragonflyEJB/locals/dropper/DropperManagerBean.java
@@ -1,5 +1,7 @@
 package fr.umlv.dragonflyEJB.locals.dropper;
 
+import java.io.File;
+
 import org.jboss.annotation.ejb.LocalBinding;
 
 import fr.umlv.dragonflyBdd.exception.DragonflyBddException;
@@ -14,15 +16,40 @@ public @Stateless class DropperManagerBean implements DropperManager {
 	}
 
 	private final static fr.umlv.dragonflyBdd.ProjectManager PM_MANAGER;
-	
+
 	static {
 		PM_MANAGER = fr.umlv.dragonflyBdd.ProjectManager.getProjectManagerInstance();
 	}
-	
+
 	public boolean removeAccount(String email) throws DragonflyBddException {
 		return AM_MANAGER.removeAccount(email);
 	}
 	public boolean removeProject(String name) throws DragonflyBddException {
+		File source = new File("Dragonfly/"+name);
+		deleteDirectory(source);
 		return PM_MANAGER.removeProject(name);
 	}
+
+
+	/**
+	 * Remove a directory
+	 * @param path the path of the directory
+	 * @return true - directory removed, false otherwise
+	 */
+	private boolean deleteDirectory(File path) { 
+		boolean resultat = true; 
+		if( path.exists() ) { 
+			File[] files = path.listFiles(); 
+			for(int i=0; i<files.length; i++) { 
+				if(files[i].isDirectory()) { 
+					resultat &= deleteDirectory(files[i]); 
+				} 
+				else { 
+					resultat &= files[i].delete(); 
+				} 
+			} 
+		} 
+		resultat &= path.delete(); 
+		return( resultat ); 
+	} 
 }
